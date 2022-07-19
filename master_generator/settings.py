0b33ca46118bac90b523f1018679cf0a01db6d5d
Javier Madriz <javiermadriz3@gmail.com>
2022-07-18 23:03:13 -0500

    Uso de libreria OS

--- master_generator/settings.py
+++ master_generator/settings.py
@@ -127,8 +127,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
-STATIC_URL = 'static/'
-STATIC_ROOT = BASE_DIR / 'static' #agregado por mi
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles') #agregado por mi
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
